240eeb91580fa942ef2bf185dce3c257660a7546
cforsgren28 <c28caden.forsgren@afacademy.af.edu>
2026-02-23 23:17:42 -0700

    final lab2

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -40,16 +40,16 @@ architecture Behavioral of sevenseg_decoder is
 
 begin
     with i_Hex select
-        o_seg_n <= "0111111" when "0000",
-                   "0000110" when "0001",
-                   "1011011" when "0010",
-                   "1001111" when "0011",
-                   "1100110" when "0100",
-                   "1101101" when "0101",
-                   "1111101" when "0110",
-                   "0000111" when "0111",
-                   "1111111" when "1000",
-                   "1100111" when "1001",
-                   "0000000" when others;
+        o_seg_n <= "1000000" when "0000",
+                   "1111001" when "0001",
+                   "0100100" when "0010",
+                   "0110000" when "0011",
+                   "0011001" when "0100",
+                   "0010010" when "0101",
+                   "0000010" when "0110",
+                   "1111000" when "0111",
+                   "0000000" when "1000",
+                   "0011000" when "1001",
+                   "1111111" when others;
                     
 end Behavioral;
